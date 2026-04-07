b8de1343053a43ca53e3b8bfd5222a8757811ad0
Vladimir <klchnv@mail.ru>
2026-04-07 10:41:41 +0300

    В транслятор добавлена постобработка

--- translator.py
+++ translator.py
@@ -1,3 +1,4 @@
+import re
 def Mul(x,a,b,Tr):
     res = "?"
     t_a = Tr[a]
@@ -9,6 +10,7 @@ def Mul(x,a,b,Tr):
         res = f"{x}.MulVec({a},{b})\n"
     if t_a == 'VECTOR' and t_b == 'VECTOR':
         res = f"{x} := mat.Dot({a},{b})\n"
+        refs.append(x)
     return res
 
 def Add(x,a,b,Tr):
@@ -48,6 +50,7 @@ def MulElem(x,a,b,Tr):
         res = f"{x}.MulElem({a},{b})\n"
     elif (t_a == 'SCALAR' or t_a == 'NUMBER') and (t_b == 'SCALAR' or t_b == 'NUMBER'):
         res = f"{x} := {a} * {b}\n"
+
     return res
 
 
@@ -65,6 +68,7 @@ def Exp(x,a,b,Tr):
 def Det(x,a,b,Tr):
     res = "?"
     t_type = Tr[x]
+
     return f"{x} := mat.Det({a})\n"
 def Inv(x,a,b,Tr):
     res = "?"
@@ -74,6 +78,7 @@ def Inv(x,a,b,Tr):
 def T(x,a,b,Tr):
     res = "?"
     t_type = Tr[x]
+    refs.append(x)
     return f"{x} := {a}.T()\n"
 def Power(x,a,b,Tr):
     res = '?'
@@ -84,6 +89,7 @@ def Power(x,a,b,Tr):
         res = f"{x}.Power({a},{b})\n"
     elif t_a == 'SCALAR' or t_a == 'NUMBER':
         res = f"{x} := ({a})**({b})\n"
+
     return res
 operations = {
     '@' : Mul,
@@ -99,9 +105,11 @@ operations = {
 
 }
 
+refs = []
 def translate(R,Tr):
     code = ''
     # print(Tr[3.0])
+
     for r in R:
         func = operations[r[3]]
 
@@ -111,13 +119,17 @@ def translate(R,Tr):
             t_type = "NONE"
 
         code += func(r[0],r[1],r[2],Tr)
+    for x in refs:
+        # code = code.replace(x,"&"+x)
+        rx = re.compile(rf"({x})(?=,|\))")
+        code = rx.sub(r"&\1",code)
     return code
 
 def test_operations():
     R=[['X1','A','B','+'],
        ['X2','X1','C','@']]
     code = translate(R)
-    print(code)
+    # print(code)
 
 
 if __name__ == "__main__":
